61ea52abe3e93482a511cbf89b42b3f2f1b030f7
cjy513203427 <634208959@qq.com>
2018-05-10 21:14:15 +0800

    正则修改

--- downloadDytt/__init__.py
+++ downloadDytt/__init__.py
@@ -17,12 +17,13 @@ def openSearchResult():
     list = search()
     req = urllib2.Request('http://www.ygdy8.com'+list[0])
     html = opener.open(req).read().decode('gb2312','ignore')
-    reg = u'ftp://[a-z0-9]+:[a-z0-9]+@[a-z0-9]+.[a-z]{1,8}.[a-z]{3}:[\d]{4}/[\u4e00-\u9fa5]{0,10}[\W]*\[阳光电影www.ygdy8.com\][\u4e00-\u9fa5]*[\d]+[\u4e00-\u9fa5]\[[\u4e00-\u9fa5]+\].rmvb'
+    reg = u'ftp://[a-z0-9]+:[a-z0-9]+@[a-z0-9]+.[a-z]{1,8}.[a-z]{3}:[\d]{4}/[\u4e00-\u9fa5]{0,10}[\W]*\[[\u4e00-\u9fa5]{0,10}www.[\w]{0,10}.[a-z]{3}\][\u4e00-\u9fa5]*[\d]+[\u4e00-\u9fa5]\[[\u4e00-\u9fa5]+\].rmvb'
     return re.findall(reg,html)
-openSearchResult()
+print openSearchResult()
 #ftp://dygod1:dygod1@y068.dydytt.net:1001/傲骨贤妻第六季/[阳光电影www.ygdy8.com]傲骨贤妻第六季第01集[中英双字].rmvb
 #ftp://dygod2:dygod2@y009.dygod.org:1004/行尸走肉第五季/[阳光电影www.ygdy8.com]行尸走肉第五季第01集[中英双字].rmvb
 #ftp://ygdy8:ygdy8@yg90.dydytt.net:2048/[阳光电影www.ygdy8.com]行尸走肉第八季第09集[中英双字].rmvb
+#ftp://dygod1:dygod1@d315.dygod.org:5033/傲骨贤妻第三季/[电影天堂www.dy2018.net]傲骨贤妻第三季21集[中英双字].rmvb
 #[\u4e00-\u9fa5]中文匹配
 def getList():
     for i in openSearchResult():
